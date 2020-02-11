ebea62b874020e0651729b26c1bcb36258d53610
AmirHossein <amirimani491@gmail.com>
2020-02-11 16:49:58 +0330

    fixing urls

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-# app_name = 'mainApp'
+app_name = 'mainApp'
 urlpatterns = [
-    path('main/', views.mainPage, name='mainpage'),
+    path('', views.mainPage, name='mainpage'),
     path('post/', views.postPage, name='postpage')
 ]
