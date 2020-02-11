26ceea4e04939526450f6c4b38122015a68a05a5
AmirHossein <amirimani491@gmail.com>
2020-02-11 16:19:58 +0330

    post model created

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+    date_of_creation = models.TimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
