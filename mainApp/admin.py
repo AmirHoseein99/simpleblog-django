91cd4a1a517df2c49d271c4ad3cb95d46519a5a9
AmirHossein <amirimani491@gmail.com>
2020-02-11 18:03:14 +0330

    ordering admin page

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'slug', 'author', 'date_of_creation')
+    list_filter = ('status', 'created', 'date_of_creation', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author')
+    date_hierarchy = 'date_of_creation'
+    ordering = ('date_of_creation')
--- mainApp/models.py
+++ mainApp/models.py
@@ -9,8 +9,11 @@ class Post(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
     slug = models.SlugField(max_length=200, unique=True)
-    date_of_creation = models.TimeField(auto_now=True)
+    date_of_creation = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ('-date_of_creation',)
